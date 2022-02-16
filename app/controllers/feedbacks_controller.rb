b586a23922c9fe942f512e7f3e9988cad9b5ffbd
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-02-16 16:15:30 +0300

    feedbacks completion

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,13 +1,12 @@
 class FeedbacksController < ApplicationController
-  # before_action :feedback_params, only: []
 
   def new
     @feedback = Feedback.new
   end
   def create
-    new_feedback = current_user.feedbacks.new(feedback_params)
+    @feedback = current_user.feedbacks.new(feedback_params)
 
-    if new_feedback.save
+    if @feedback.save
       redirect_to tests_path
     else
       render :new
