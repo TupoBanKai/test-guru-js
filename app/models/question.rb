9de964e4ea3b044d89541e6064d397ad45bc1959
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-02-16 16:57:59 +0300

    even more fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,12 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :gists
+<<<<<<< Updated upstream
   has_many :answers
   has_many :test_passages
+=======
+  has_many :answers, dependent: :destroy
+>>>>>>> Stashed changes
 
   validates :body, presence: true
 
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  has_many :test_passages
-  has_many :questions
+  has_many :test_passages, dependent: :destroy
+  has_many :questions, dependent: :destroy
   has_many :users, through: :test_passages
   belongs_to :category
   belongs_to :creator, class_name: 'User', foreign_key: :creator_id
