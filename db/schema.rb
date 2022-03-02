eed6abfa9ebf835229ec943bd9f2743a9b4a4864
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-03-02 19:06:56 +0300

    feedbacks

--- db/schema.rb
+++ db/schema.rb
@@ -112,12 +112,4 @@ ActiveRecord::Schema.define(version: 2022_02_14_140245) do
     t.index ["type"], name: "index_users_on_type"
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "gists", "questions"
-  add_foreign_key "gists", "users"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "questions", column: "current_question_id"
-  add_foreign_key "test_passages", "tests"
-  add_foreign_key "test_passages", "users"
-  add_foreign_key "tests", "categories"
 end
