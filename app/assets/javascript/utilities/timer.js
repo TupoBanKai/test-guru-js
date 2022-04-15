b4bbe182e608d98557259eebef0383978296789d
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-04-15 18:36:20 +0300

    homework

--- app/assets/javascript/utilities/timer.js
+++ app/assets/javascript/utilities/timer.js
@@ -1,12 +1,17 @@
 document.addEventListener('turbolinks:load', function() {
- var i  = document.getElementById('time')
+ var i = document.getElementById('minute').innerHTML;
+})
+var floor = 0
 function counter(){
+  var i = document.getElementById('minute').innerHTML * 60;
   if (i <= 0) {
     clearTimeout(timer);
+    document.forms[0].submit();
   } else {
-    i -= 1;
-    document.getElementById('time').innerHTML = i
+    i = (i + floor) - 1;
+    floor = i - Math.floor(i / 60) * 60
+    document.getElementById('minute').innerHTML = Math.floor(i / 60)
+    document.getElementById('second').innerHTML = i % 60
   }
 }
-counter();
-var timer = setInterval(r, 1000);
+var timer = setInterval(counter, 1000);
