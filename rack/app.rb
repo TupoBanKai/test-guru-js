d22fbcb77eeb14860fb9b9427b745d551982cb30
TupoBanKai <leagueoflegends.talk@gmail.com>
2022-07-04 22:38:39 +0300

    edits

--- rack/app.rb
+++ rack/app.rb
@@ -1,19 +1,12 @@
 require_relative "date"
 class App
-  CONTENT_TYPE = {"Content-Type" => "text/plain"}
-
   def call(env)
-    date = DateService.new()
     req = Rack::Request.new(env)
     if link_check(req)
-      if date.success?
-        date.date_format_check(env["QUERY_STRING"].split("."))
-        response("#{DateTime.now.strftime(date.date_for_user)}", 200, CONTENT_TYPE)
-      else
-        response("Unknown time format #{date.errors} ", 400, CONTENT_TYPE)
-      end
+      date = DateService.new()
+      response_qualifier(date)
     else
-      response("Not found", 404, CONTENT_TYPE)
+      response("Not found", 404)
     end
   end
 
@@ -21,8 +14,17 @@ class App
     req.path == "/date"
   end
 
-  def response(body, status, headers)
-    response = Rack::Response.new(body, status, headers)
+  def response_qualifier(date)
+    if date.success?
+      date.date_format_check(env["QUERY_STRING"].split("."))
+      response("#{DateTime.now.strftime(date.date_for_user)}", 200)
+    else
+      response("Unknown time format #{date.errors} ", 400)
+    end
+  end
+
+  def response(body, status)
+    response = Rack::Response.new(body, status, {"Content-Type" => "text/plain"})
     response.finish
   end
 end
